3e80b368a8d392ad65138f73cec13b5c2d6e6b4c
Jefwillems <jefwillems100@gmail.com>
2025-08-13 17:39:36 +0200

    install argo with helm chart

--- setup.sh
+++ setup.sh
@@ -12,24 +12,26 @@ kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
 echo "[3/8] Installing sealed-secrets..."
 helm install sealed-secrets -n kube-system --set-string fullnameOverride=sealed-secrets-controller sealed-secrets/sealed-secrets
 
+echo "[3.5/8] Waiting for sealed-secrets controller to be ready..."
+kubectl wait --for=condition=Available --timeout=120s deployment/sealed-secrets-controller -n kube-system
+kubectl wait --for=condition=Ready --timeout=120s pod -l app.kubernetes.io/name=sealed-secrets -n kube-system
+
 echo "[4/8] Creating argocd namespace..."
 kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
 
-echo "[5/8] Installing ArgoCD..."
-# kubectl apply -k ./infra/argocd/install/ -n argocd --wait=true
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml --wait=true
+# echo "[5/8] Installing ArgoCD..."
+# # kubectl apply -k ./infra/argocd/install/ -n argocd --wait=true
+# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml --wait=true
 
-echo "[6/8] Waiting for ArgoCD pods to be ready..."
-kubectl wait --for=condition=Available --timeout=180s deployment -l app.kubernetes.io/part-of=argocd -n argocd
-kubectl wait --for=condition=Ready --timeout=180s pod -l app.kubernetes.io/name -n argocd
+# echo "[6/8] Waiting for ArgoCD pods to be ready..."
+# kubectl wait --for=condition=Available --timeout=180s deployment -l app.kubernetes.io/part-of=argocd -n argocd
+# kubectl wait --for=condition=Ready --timeout=180s pod -l app.kubernetes.io/name -n argocd
 
-kubectl -n argocd patch secret argocd-secret \
-  -p '{"stringData": {
-    "admin.password": "$2a$12$XBVWOw4ob/xRu/taWesu2Oni9OebnoFjKUo4ReaNTBZtr4BL..ybO",
-    "admin.passwordMtime": "'$(date +%FT%T%Z)'"
-  }}'
-
-
+# kubectl -n argocd patch secret argocd-secret \
+#   -p '{"stringData": {
+#     "admin.password": "$2a$12$XBVWOw4ob/xRu/taWesu2Oni9OebnoFjKUo4ReaNTBZtr4BL..ybO",
+#     "admin.passwordMtime": "'$(date +%FT%T%Z)'"
+#   }}'
 
 echo "[7/8] Sealing repo credentials..."
 if [ -f ./infra/argocd/repo-creds-unsafe.yaml ]; then
@@ -45,11 +47,12 @@ fi
 # kubectl create namespace istio-system --dry-run=client -o yaml | kubectl apply -f -
 
 echo "[8/8] Bootstrapping ArgoCD app of apps..."
+
+helm upgrade --install argocd ./infra/argocd/install --namespace argocd --values ./infra/argocd/install/values.yaml
 kubectl apply -f infra/argocd/repo-creds.yaml
 kubectl apply -f infra/argocd/repositories.yaml
 kubectl apply -f infra/argocd/applications.yaml
 kubectl apply -f infra/argocd/infra-set.yaml
-
 
 echo "Waiting for LoadBalancer services to be assigned external IPs..."
 # timeout 180 bash -c 'until kubectl get svc --all-namespaces -o json | jq -e ".items[] | select(.spec.type==\"LoadBalancer\") | .status.loadBalancer.ingress[0].ip or .status.loadBalancer.ingress[0].hostname"; do echo waiting for LoadBalancer IPs...; sleep 5; done'
