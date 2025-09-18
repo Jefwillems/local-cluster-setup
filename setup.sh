f80a51ce86007dd00f44116f71725909b5c24bef
Jefwillems <jefwillems100@gmail.com>
2025-09-18 16:19:57 +0200

    feat: add app-of-apps configuration and update setup script to apply it

--- setup.sh
+++ setup.sh
@@ -72,6 +72,7 @@ echo -e "\033[1;34m[INFO][8/8] Bootstrapping ArgoCD app of apps...\033[0m"
 
 helm upgrade --install argocd ./infra/argocd/install --namespace argocd --values ./infra/argocd/install/values.yaml
 kubectl apply -f infra/argocd/app-of-infras.yaml
+kubectl apply -f infra/argocd/app-of-apps.yaml
 
 echo -e "\033[1;34m[INFO] Running cloud-provider-kind with lb port mapping enabled...\033[0m"
 cloud-provider-kind -enable-lb-port-mapping
